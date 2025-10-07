008f1fc2a0e5ffa8be9c5d53b4c23763cd14c75d
Marcio Sardou <marciosut1996@gmail.com>
2025-10-06 21:14:29 -0300

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Portfolio - Marcio Sardou',
+  description: 'My portfolio showcasing my projects and skills as a Frontend Developer.',
   generator: 'v0.app',
 }
 
